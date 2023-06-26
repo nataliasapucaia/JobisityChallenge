c158e17158a50457cfa4f0d8ce20989144bcaa2a
Natália Sapucaia <natisapucaia@gmail.com>
2023-06-26 12:44:03 -0300

    First commit

---
+++ JobsityStreaming/JobsityStreamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsityStreamingApp.swift
+//  JobsityStreaming
+//
+//  Created by Natália Sapucaia on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct JobsityStreamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
